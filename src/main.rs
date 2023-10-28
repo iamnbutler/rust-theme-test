978a1d6899bff598d2924b627e8e358312358fa7
Nate Butler <iamnbutler@gmail.com>
2023-10-27 21:54:37 -0400

    Add 'anyhow' dependency, revise theme structures to use Cow, and improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,65 @@
+use std::borrow::Cow;
+use anyhow::{Result, Context};
+
 pub trait Theme {
     fn colors(&self) -> &ThemeColors;
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
-pub struct ThemeColors {
-    pub fg: String,
-    pub bg: String,
-    pub border: String,
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+pub struct ThemeColors<'a> {
+    pub fg: Cow<'a, str>,
+    pub bg: Cow<'a, str>,
+    pub border: Cow<'a, str>,
 }
 
-pub struct SystemTheme {
-    colors: ThemeColors,
+pub struct SystemTheme<'a> {
+    colors: ThemeColors<'a>,
 }
 
-impl SystemTheme {
+impl<'a> SystemTheme<'a> {
     fn new() -> Self {
         SystemTheme {
             colors: ThemeColors {
-                fg: "white".into(),
-                bg: "blue".into(),
-                border: "gray".into(),
-            }
+                fg: Cow::Borrowed("white"),
+                bg: Cow::Borrowed("blue"),
+                border: Cow::Borrowed("gray"),
+            },
         }
     }
 }
 
-pub struct UserTheme {
-    colors: ThemeColors,
+pub struct UserTheme<'a> {
+    colors: ThemeColors<'a>,
 }
 
-impl UserTheme {
-    fn from_file(path: &str) -> std::io::Result<Self> {
-        let file = std::fs::File::open(path)?;
+impl<'a> UserTheme<'a> {
+    fn from_file(path: &str) -> Result<Self> {
+        let file = std::fs::File::open(path).context("Unable to open theme file")?;
         let reader = std::io::BufReader::new(file);
-        let colors: ThemeColors = serde_json::from_reader(reader)?;
+        let colors: ThemeColors = serde_json::from_reader(reader).context("Failed to deserialize theme")?;
         Ok(UserTheme { colors })
     }
 }
 
-impl Theme for UserTheme {
+impl<'a> Theme for UserTheme<'a> {
     fn colors(&self) -> &ThemeColors {
         &self.colors
     }
 }
 
-impl Theme for SystemTheme {
+impl<'a> Theme for SystemTheme<'a> {
     fn colors(&self) -> &ThemeColors {
         &self.colors
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> anyhow::Result<()> {
     let system_theme = SystemTheme::new();
     println!("System Theme:");
     print_theme_colors(system_theme.colors());
 
     println!("\nUser Theme:");
-    let user_theme = UserTheme::from_file("test_theme.json")?;
+    let user_theme = UserTheme::from_file("theme/test_theme.json")?;
     print_theme_colors(user_theme.colors());
 
     Ok(())
